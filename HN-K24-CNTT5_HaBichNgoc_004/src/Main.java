ca1ebd3ca55a7abafdec6463c234f99501afa1e8
Bichngocc07 <ngoc01072006@gmail.com>
2026-03-19 10:30:32 +0700

    Add files via upload

--- HN-K24-CNTT5_HaBichNgoc_004/src/Main.java
+++ HN-K24-CNTT5_HaBichNgoc_004/src/Main.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void Main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int choice;
-        do {
+        CourseBusiness bus = new CourseBusiness();
+
+        while (true) {
             System.out.println("==========QUẢN LÝ KHÓA HỌC==========");
             System.out.println("1. Hiển thị danh sách toàn bộ khóa học");
             System.out.println("2. Thêm mới khóa học");
@@ -14,28 +15,39 @@ public class Main {
             System.out.println("6. Lọc khóa học đang mở");
             System.out.println("7. Sắp xếp khóa học theo học phí giảm dần");
             System.out.println("8. Thoát");
-            choice = sc.nextInt();
-            switch (choice){
-                case 1 :
+            int choice;
+
+            try {
+                choice = Integer.parseInt(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Sai dữ liệu");
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    bus.displayAll();
                     break;
-                case 2 :
+                case 2:
+                    bus.addCourse(sc);
                     break;
-                case 3 :
+                case 3:
+                    bus.updateCourse(sc);
                     break;
-                case 4 :
+                case 4:
+                    bus.deleteCourse(sc);
                     break;
-                case 5 :
+                 case 5:
                     break;
-                case 6 :
+                case 6:
                     break;
-                case 7 :
+                case 7:
                     break;
-                case 8 :
-                    System.out.println("Thoát ");
+                case 8:
                     break;
                 default:
-                    System.out.println("Lựa chọn của bạn hợp lệ");
+                    System.out.println("Chỉ chọn 1-4");
             }
-        }while (true);
+        }
     }
 }
